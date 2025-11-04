6cf0dd0b025ef15bf5491f9e10cb7700269f7e4d
davidmesquita849-code <davidmesquita849@gmail.com>
2025-11-04 16:10:41 +0100

    post integriert

---
+++ tailwind.config.js/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        reload: "#C8102E"
+      }
+    },
+  },
+  plugins: [],
+};
